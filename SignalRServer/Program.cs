84e5085775aa032d6a560b2ba671ee40c28c2e95
Kenny Pflug <kenny.pflug@live.de>
2015-05-13 22:07:01 +0200

    added SignalR console service

---
+++ SignalRServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Configuration;
+
+namespace SignalRServer
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var targetPort = Convert.ToInt32(ConfigurationManager.AppSettings["Port"]);
+            var baseAddress = string.Format("http://{0}:{1}", Environment.MachineName, targetPort);
+
+            StartService:
+            using (WebApp.Start(baseAddress, Configure))
+            {
+                Console.WriteLine("SignalR service running on " + baseAddress);
+                Console.WriteLine("Press ENTER to shutdown the service...");
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Service shut down.");
+            Console.WriteLine("Press ENTER to restart the service");
+            Console.WriteLine("Press ESCAPE to quit");
+
+            while (true)
+            {
+                var keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    goto StartService;
+                }
+                if(keyInfo.Key == ConsoleKey.Escape)
+                    break;
+            }
+        }
+
+        private static void Configure(IAppBuilder appBuilder)
+        {
+            appBuilder.MapSignalR();
+        }
+    }
+}
---
+++ SignalRServer/SampleHub.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRServer
+{
+    public class SampleHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            LogToConsole(string.Format("{0} send \"{1}\"", Context.ConnectionId, message));
+        }
+
+        public override Task OnConnected()
+        {
+            LogToConsole(Context.ConnectionId + " connected", ConsoleColor.Green);
+            return base.OnConnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            LogToConsole(Context.ConnectionId + " reconnected", ConsoleColor.Yellow);
+            return base.OnReconnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            if (stopCalled)
+                LogToConsole(Context.ConnectionId + " disconnected on purpose.", ConsoleColor.Red);
+            else
+                LogToConsole(Context.ConnectionId + " lost connection.", ConsoleColor.DarkRed);
+            return base.OnDisconnected(stopCalled);
+        }
+
+        private static void LogToConsole(string message, ConsoleColor color)
+        {
+            var previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            LogToConsole(message);
+            Console.ForegroundColor = previousColor;
+        }
+
+        private static void LogToConsole(string message)
+        {
+            Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + message);
+        }
+    }
+}
