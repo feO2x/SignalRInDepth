392779661e1ae84380678981384799ff4df2f2d8
Kenny Pflug <kenny.pflug@live.de>
2015-05-26 17:08:06 +0200

    added test showing that CreateHubProxy returns the same instance on subsequent calls

---
+++ SignalRAutomatedTests/ClientConnectionTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.AspNet.SignalR.Client;
+using Xunit;
+
+namespace SignalRAutomatedTests
+{
+    public sealed class ClientConnectionTests
+    {
+        [Fact]
+        public void HubConnectionReturnsSameInstancesOnSubsequentCreateHubProxyCalls()
+        {
+            var hubConnection = new HubConnection("Foo");
+
+            var firstProxy = hubConnection.CreateHubProxy("Baz");
+            var secondProxy = hubConnection.CreateHubProxy("Baz");
+
+            firstProxy.Should().BeSameAs(secondProxy);
+        }
+    }
+}
